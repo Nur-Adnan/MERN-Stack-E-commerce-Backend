e2fe41c454761e425ce7fdff64139944f526099e
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-23 14:11:33 +0600

    admin-routes-completed

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,10 +1,11 @@
+import { adminOnly } from './../middlewares/auth.js';
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
 const app = express.Router();
 // route - /api/v1/user/new
 app.post("/new", newUser);
 // Route - /api/v1/user/all
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 // Route - /api/v1/user/dynamicID -> Make it Chaining
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 export default app;

364508ac841c9f1f40848836bd05036da06be02f
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-24 20:11:52 +0600

    getAllProducts api's sorting functionality integrated

--- dist/controllers/product.js
+++ dist/controllers/product.js
@@ -101,6 +101,9 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
 export const getAllProducts = TryCatch(async (req, res, next) => {
     const { search, sort, category, price } = req.query;
     const page = Number(req.query.page) || 1;
+    // 1,2,3,4,5,6,7,8
+    // 9,10,11,12,13,14,15,16
+    // 17,18,19,20,21,22,23,24
     const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
     const skip = (page - 1) * limit;
     const baseQuery = {};
@@ -115,7 +118,10 @@ export const getAllProducts = TryCatch(async (req, res, next) => {
         };
     if (category)
         baseQuery.category = category;
-    const products = await Product.find(baseQuery);
+    const products = await Product.find(baseQuery)
+        .sort(sort && { price: sort === "asc" ? 1 : -1 })
+        .limit(limit)
+        .skip(skip);
     return res.status(200).json({
         success: true,
         products,
--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -130,8 +130,11 @@ export const getAdminProducts = TryCatch(async (req, res, next) => {
 
       const page = Number(req.query.page) || 1;
 
-        const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
-        const skip = (page - 1) * limit;
+    // 1,2,3,4,5,6,7,8
+    // 9,10,11,12,13,14,15,16
+    // 17,18,19,20,21,22,23,24
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
 
     const baseQuery: BaseQuery = {};
 
@@ -147,7 +150,11 @@ export const getAdminProducts = TryCatch(async (req, res, next) => {
         };
 
     if (category) baseQuery.category = category;
-    const products = await Product.find(baseQuery);
+
+    const products = await Product.find(baseQuery)
+    .sort(sort && { price: sort === "asc" ? 1 : -1 })
+    .limit(limit)
+    .skip(skip);
 
 
     return res.status(200).json({

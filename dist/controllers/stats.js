4304469fa0ac74b43ba5852f571a03366713888f
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-26 03:35:05 +0600

    getDashboardStats controller building started

--- dist/controllers/stats.js
+++ dist/controllers/stats.js
@@ -1,1 +1,16 @@
-export {};
+import { myCache } from "../app.js";
+import { TryCatch } from "../middlewares/error.js";
+export const getDashboardStats = TryCatch(async (req, res, next) => {
+    let stats;
+    if (myCache.has("admin-stats"))
+        stats = JSON.parse(myCache.get("admin-stats"));
+    else {
+    }
+    return res.status(200).json({
+        success: true,
+        stats
+    });
+});
+export const getPieCharts = TryCatch(async () => { });
+export const getBarCharts = TryCatch(async () => { });
+export const getLineCharts = TryCatch(async () => { });

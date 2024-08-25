b3955ba03fa1b46794cf4a65cdf85d54428610e9
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-26 00:39:03 +0600

    myOders route completed and allOrders route started

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "../middlewares/error.js";
 import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
+import { myCache } from "../app.js";
 
 export const newOrder = TryCatch(
     async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
@@ -21,7 +22,7 @@ export const newOrder = TryCatch(
       if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
         return next(new ErrorHandler("Please Enter All Fields", 400));
 
-      const order = await Order.create({
+      await Order.create({
         shippingInfo,
         orderItems,
         user,
@@ -48,3 +49,25 @@ export const newOrder = TryCatch(
       });
     }
   );
+
+
+  export const myOrders = TryCatch(async (req, res, next) => {
+    const { id: user } = req.query;
+    let orders = [];
+
+    const key = `my-orders-${user}`;
+
+    if(myCache.has(key)) orders = JSON.parse(myCache.get("") as string);
+    else {
+        orders = await Order.find({user})
+        myCache.set(key, JSON.stringify(orders));
+    }
+
+    return res.status(200).json({
+      success: true,
+      orders,
+    });
+  });
+
+
+
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import {
+    allOrders,
+    myOrders,
 //   allOrders,
 //   deleteOrder,
 //   getSingleOrder,
 //   myOrders,
-  newOrder,
+    newOrder,
 //   processOrder,
 } from "../controllers/order.js";
 
@@ -14,11 +16,11 @@ const app = express.Router();
 // route - /api/v1/order/new
 app.post("/new", newOrder);
 
-// // route - /api/v1/order/my
-// app.get("/my", myOrders);
+// route - /api/v1/order/my
+app.get("/my", myOrders);
 
-// // route - /api/v1/order/my
-// app.get("/all", adminOnly, allOrders);
+// route - /api/v1/order/my
+app.get("/all", adminOnly, allOrders);
 
 // app
 //   .route("/:id")

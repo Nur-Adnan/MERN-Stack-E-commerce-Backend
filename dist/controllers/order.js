27456905a8c7ffcfcdc9f2da69bf734d5e601153
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-26 01:06:14 +0600

    all some professional sequence

--- dist/controllers/order.js
+++ dist/controllers/order.js
@@ -3,33 +3,6 @@ import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "../middlewares/error.js";
 import { Order } from "../models/order.js";
 import { myCache } from "../app.js";
-export const newOrder = TryCatch(async (req, res, next) => {
-    const { shippingInfo, orderItems, user, subtotal, tax, shippingCharges, discount, total, } = req.body;
-    if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
-        return next(new ErrorHandler("Please Enter All Fields", 400));
-    await Order.create({
-        shippingInfo,
-        orderItems,
-        user,
-        subtotal,
-        tax,
-        shippingCharges,
-        discount,
-        total,
-    });
-    await reduceStock(orderItems);
-    await invalidateCache({
-        product: true,
-        order: true,
-        admin: true,
-        // userId: user,
-        // productId: order.orderItems.map((i) => String(i.productId)),
-    });
-    return res.status(201).json({
-        success: true,
-        message: "Order Placed Successfully",
-    });
-});
 export const myOrders = TryCatch(async (req, res, next) => {
     const { id: user } = req.query;
     let orders = [];
@@ -74,5 +47,32 @@ export const getSingleOrder = TryCatch(async (req, res, next) => {
     return res.status(200).json({
         success: true,
         order,
+    });
+});
+export const newOrder = TryCatch(async (req, res, next) => {
+    const { shippingInfo, orderItems, user, subtotal, tax, shippingCharges, discount, total, } = req.body;
+    if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
+        return next(new ErrorHandler("Please Enter All Fields", 400));
+    await Order.create({
+        shippingInfo,
+        orderItems,
+        user,
+        subtotal,
+        tax,
+        shippingCharges,
+        discount,
+        total,
+    });
+    await reduceStock(orderItems);
+    await invalidateCache({
+        product: true,
+        order: true,
+        admin: true,
+        // userId: user,
+        // productId: order.orderItems.map((i) => String(i.productId)),
+    });
+    return res.status(201).json({
+        success: true,
+        message: "Order Placed Successfully",
     });
 });
--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -6,51 +6,6 @@ import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { myCache } from "../app.js";
 
-export const newOrder = TryCatch(
-    async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
-      const {
-        shippingInfo,
-        orderItems,
-        user,
-        subtotal,
-        tax,
-        shippingCharges,
-        discount,
-        total,
-      } = req.body;
-
-      if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
-        return next(new ErrorHandler("Please Enter All Fields", 400));
-
-      await Order.create({
-        shippingInfo,
-        orderItems,
-        user,
-        subtotal,
-        tax,
-        shippingCharges,
-        discount,
-        total,
-      });
-
-      await reduceStock(orderItems);
-
-      await invalidateCache({
-        product: true,
-        order: true,
-        admin: true,
-        // userId: user,
-        // productId: order.orderItems.map((i) => String(i.productId)),
-      });
-
-      return res.status(201).json({
-        success: true,
-        message: "Order Placed Successfully",
-      });
-    }
-  );
-
-
   export const myOrders = TryCatch(async (req, res, next) => {
     const { id: user } = req.query;
     let orders = [];
@@ -105,3 +60,48 @@ export const newOrder = TryCatch(
       order,
     });
   });
+
+
+  export const newOrder = TryCatch(
+    async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
+      const {
+        shippingInfo,
+        orderItems,
+        user,
+        subtotal,
+        tax,
+        shippingCharges,
+        discount,
+        total,
+      } = req.body;
+
+      if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
+        return next(new ErrorHandler("Please Enter All Fields", 400));
+
+      await Order.create({
+        shippingInfo,
+        orderItems,
+        user,
+        subtotal,
+        tax,
+        shippingCharges,
+        discount,
+        total,
+      });
+
+      await reduceStock(orderItems);
+
+      await invalidateCache({
+        product: true,
+        order: true,
+        admin: true,
+        // userId: user,
+        // productId: order.orderItems.map((i) => String(i.productId)),
+      });
+
+      return res.status(201).json({
+        success: true,
+        message: "Order Placed Successfully",
+      });
+    }
+  );

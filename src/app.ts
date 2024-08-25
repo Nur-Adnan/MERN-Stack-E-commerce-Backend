5cb5154c243c88cff40dbc4d55f3e7204531f1e2
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-25 13:45:38 +0600

    order api building started

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import NodeCache from "node-cache";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
+import orderRoute from "./routes/order.js";
+// import paymentRoute from "./routes/payment.js";
+// import dashboardRoute from "./routes/stats.js";
 
 
 const port = 3000;
@@ -18,9 +21,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API Working with /api/v1");
 })
+
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+// app.use("/api/v1/payment", paymentRoute);
+// app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware)

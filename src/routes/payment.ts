266f5503d6f9a285575c50c7d292859d498924b9
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-26 02:29:11 +0600

    payment controller all functionalities integration successfull

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import {
-//   allCoupons,
+  allCoupons,
   applyDiscount,
 //   createPaymentIntent,
-//   deleteCoupon,
-//   getCoupon,
+  deleteCoupon,
+  getCoupon,
   newCoupon,
-//   updateCoupon,
+  updateCoupon,
 } from "../controllers/payment.js";
 
 const app = express.Router();
@@ -19,16 +19,16 @@ const app = express.Router();
 app.get("/discount", applyDiscount);
 
 // route - /api/v1/payment/coupon/new
-app.post("/coupon/new", newCoupon);
+app.post("/coupon/new", adminOnly, newCoupon);
 
 // route - /api/v1/payment/coupon/all
-// app.get("/coupon/all", adminOnly, allCoupons);
+app.get("/coupon/all", adminOnly, allCoupons);
 
 // route - /api/v1/payment/coupon/:id
-// app
-//   .route("/coupon/:id")
-//   .get(adminOnly, getCoupon)
-//   .put(adminOnly, updateCoupon)
-//   .delete(adminOnly, deleteCoupon);
+app
+  .route("/coupon/:id")
+  .get(adminOnly, getCoupon)
+  .put(adminOnly, updateCoupon)
+  .delete(adminOnly, deleteCoupon);
 
 export default app;
